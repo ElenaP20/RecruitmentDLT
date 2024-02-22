8f0fd750005f8d194dcff10d8262bc9801e5b9a5
ElenaP20 <91874987+ElenaP20@users.noreply.github.com>
2024-02-22 16:15:13 +0000

    Working double encryption

--- CVencryption.py
+++ CVencryption.py
@@ -1,17 +1,18 @@
-from Crypto.Cipher import AES, PKCS1_v1_5
+from Crypto.Cipher import AES, PKCS1_OAEP
 from Crypto.PublicKey import RSA
 from Crypto.Random import get_random_bytes
 from Crypto.Util.Padding import pad
-
-# Generate the secret key
-key = get_random_bytes(32)  # 256-bit key
-
-# Create the AES cipher object (no IV needed for ECB mode)
-aes_cipher = AES.new(key, AES.MODE_ECB)
+import base64
 
 # Read the XML file content
 with open("CV.xml", "rb") as f:
     xml_data = f.read()
+
+# Generate a random AES key
+aes_key = get_random_bytes(32)  # 256-bit key
+
+# Create AES cipher object
+aes_cipher = AES.new(aes_key, AES.MODE_CBC)
 
 # Pad the data to a multiple of the block size
 padded_data = pad(xml_data, AES.block_size)
@@ -19,27 +20,26 @@ padded_data = pad(xml_data, AES.block_size)
 # Encrypt the XML data using AES
 aes_encrypted_data = aes_cipher.encrypt(padded_data)
 
+# Generate a random IV for AES
+iv = aes_cipher.iv
+
 # Read the recipient's RSA public key in PEM format
-with open('recipient_public_key.pem', 'rb') as f:
+with open('public_key.pem', 'rb') as f:
     rsa_public_key_pem = f.read()
 
-try:
-    # Import the RSA public key from PEM format
-    rsa_public_key = RSA.import_key(rsa_public_key_pem)
-except ValueError as e:
-    print("Error importing RSA public key:", e)
-    print("Make sure the key format is supported and the file content is correct.")
-    # Exit or handle the error accordingly
-    exit(1)
+# Import the RSA public key from PEM format
+rsa_public_key = RSA.import_key(rsa_public_key_pem)
 
-# Create the RSA cipher object
-rsa_cipher = PKCS1_v1_5.new(rsa_public_key)
+# Create RSA cipher object
+rsa_cipher = PKCS1_OAEP.new(rsa_public_key)
 
-# Encrypt the AES-encrypted data using RSA
-rsa_encrypted_data = rsa_cipher.encrypt(aes_encrypted_data)
+# Encrypt the AES key with RSA
+encrypted_aes_key = rsa_cipher.encrypt(aes_key)
 
-# Write the doubly encrypted data to a file
+# Write encrypted AES key, IV, and AES-encrypted data to a file
 with open("doubly_encrypted_cv.dat", "wb") as f:
-    f.write(rsa_encrypted_data)
+    f.write(encrypted_aes_key)
+    f.write(iv)
+    f.write(aes_encrypted_data)
 
-print("XML file encrypted using AES-256 in ECB mode and then doubly encrypted with RSA successfully!")
+print("XML file encrypted using AES-256 in CBC mode and then doubly encrypted with RSA (using hybrid encryption) successfully!")
