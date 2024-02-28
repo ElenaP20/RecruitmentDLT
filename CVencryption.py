3795d00d4de4ad37db1edffeee16135af47e5384
ElenaP20 <91874987+ElenaP20@users.noreply.github.com>
2024-02-28 12:56:08 +0000

    OOP modification to CVencryption

--- CVencryption.py
+++ CVencryption.py
@@ -4,46 +4,45 @@ from Crypto.Random import get_random_bytes
 from Crypto.Util.Padding import pad
 import base64
 
-# Read the XML file content
-with open("CV.xml", "rb") as f:
-    xml_data = f.read()
+class Encryption:
+    def __init__(self, xml_file, public_key_file):
+        self.xml_file = xml_file
+        self.public_key_file = public_key_file
 
-# Generate a random AES key
-aes_key = get_random_bytes(32)  # 256-bit key
+    def read_xml_data(self):
+        with open(self.xml_file, "rb") as f:
+            return f.read()
 
-# Create AES cipher object
-aes_cipher = AES.new(aes_key, AES.MODE_CBC)
+    def generate_aes_key(self):
+        return get_random_bytes(32)  # 256-bit key
 
-# Pad the data to a multiple of the block size
-padded_data = pad(xml_data, AES.block_size)
+    def encrypt_aes(self, xml_data, aes_key):
+        aes_cipher = AES.new(aes_key, AES.MODE_CBC)
+        padded_data = pad(xml_data, AES.block_size)
+        return aes_cipher.encrypt(padded_data), aes_cipher.iv
 
-# Encrypt the XML data using AES
-aes_encrypted_data = aes_cipher.encrypt(padded_data)
+    def read_rsa_public_key(self):
+        with open(self.public_key_file, 'rb') as f:
+            return RSA.import_key(f.read())
 
-# Generate a random IV for AES
-iv = aes_cipher.iv
+    def encrypt_with_rsa(self, data, rsa_public_key):
+        rsa_cipher = PKCS1_OAEP.new(rsa_public_key)
+        return rsa_cipher.encrypt(data)
 
-# Read the recipient's RSA public key in PEM format
-with open('test.pem', 'rb') as f:
-    rsa_public_key_pem = f.read()
+    def encrypt_data(self):
+        xml_data = self.read_xml_data()
+        aes_key = self.generate_aes_key()
+        aes_encrypted_data, iv = self.encrypt_aes(xml_data, aes_key)
+        rsa_public_key = self.read_rsa_public_key()
+        encrypted_aes_key_part1 = self.encrypt_with_rsa(aes_key[:16], rsa_public_key)
+        encrypted_aes_key_part2 = self.encrypt_with_rsa(aes_key[16:], rsa_public_key)
 
-# Import the RSA public key from PEM format
-rsa_public_key = RSA.import_key(rsa_public_key_pem)
+        with open("packet1.dat", "wb") as f1, open("packet2.dat", "wb") as f2:
+            f1.write(iv + encrypted_aes_key_part1 + aes_encrypted_data)
+            f2.write(iv + encrypted_aes_key_part2 + aes_encrypted_data)
 
-# Create RSA cipher object
-rsa_cipher = PKCS1_OAEP.new(rsa_public_key)
+        print("Two packets with encrypted headers and AES-encrypted CV content have been created successfully!")
 
-# Encrypt the CV data with AES
-cv_encrypted_with_aes = aes_encrypted_data
-
-# Split the AES key into two halves
-half_aes_key_length = len(aes_key) // 2
-encrypted_aes_key_part1 = rsa_cipher.encrypt(aes_key[:half_aes_key_length])
-encrypted_aes_key_part2 = rsa_cipher.encrypt(aes_key[half_aes_key_length:])
-
-# Write the packets to two separate files, each containing the iv and one half of the encrypted AES key in the header
-with open("packet1.dat", "wb") as f1, open("packet2.dat", "wb") as f2:
-    f1.write(iv + encrypted_aes_key_part1 + cv_encrypted_with_aes)
-    f2.write(iv + encrypted_aes_key_part2 + cv_encrypted_with_aes)
-
-print("Two packets with encrypted headers and AES-encrypted CV content have been created successfully!")
+# Usage
+encryptor = Encryption("CV.xml", "test.pem")
+encryptor.encrypt_data()
